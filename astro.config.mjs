acab2ac8a0d78075ee8f1b8689f66cff429e9076
ayyappan97899 <ayyappan97899@gmail.com>
2024-07-02 14:14:57 +0530

    code base changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: "Vite+React", link: "/codebase/vite_react" },
+            { label: "Next", link: "/codebase/next" },
           ],
         },
         {
